3911fecb65886676c9b65217ac3ff3175fe0ea04
Jin.He <hejinsl@126.com>
2017-10-17 14:36:51 +0800

    初始化提交

---
+++ appframe/src/main/java/com/primeton/appframe/common/datasource/DynamicDataSource.java
@@ -0,0 +1,37 @@
+package com.primeton.appframe.common.datasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+
+/**
+ * 
+ * ClassName: DynamicDataSource <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON(可选). <br/>
+ * date: 2017年10月17日 下午12:15:45 <br/>
+ * 动态数据源切换
+ * @author Jin.He (mailto:hejin@primeton.com)
+ * @version
+ */
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return getDataSourceType();
+    }
+    
+    private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
+
+    public static void setDataSourceType(String dataSourceType) {
+        contextHolder.set(dataSourceType);
+    }
+
+    public static String getDataSourceType() {
+        return contextHolder.get();
+    }
+
+    public static void clearDataSourceType() {
+        contextHolder.remove();
+    }
+
+}
